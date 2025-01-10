bdd516d5e0ed9ce63963a075c355d08f8da9997b
Juan Betancur <juanfelipebetancurcano@gmail.com>
2025-01-09 21:52:13 -0500

    added prisma

---
+++ prisma/migrations/20250110020113_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Character" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "element" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Character_pkey" PRIMARY KEY ("id")
+);

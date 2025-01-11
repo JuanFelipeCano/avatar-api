94c840e76895c023196117b8b325ff858110f753
Juan Betancur <juanfelipebetancurcano@gmail.com>
2025-01-11 01:16:22 -0500

    updated unit tests

--- src/modules/skills/v1/skills.controller.spec.ts
+++ src/modules/skills/v1/skills.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SkillsController } from './skills.controller';
 import { SkillsService } from './skills.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 describe('SkillsController', () => {
   let controller: SkillsController;
@@ -8,7 +9,7 @@ describe('SkillsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SkillsController],
-      providers: [SkillsService],
+      providers: [SkillsService, PrismaService],
     }).compile();
 
     controller = module.get<SkillsController>(SkillsController);

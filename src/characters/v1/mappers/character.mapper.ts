a24d652bba34b4cbf907190804567293d665f9fa
Juan Betancur <juanfelipebetancurcano@gmail.com>
2025-01-10 23:29:20 -0500

    added mappers

---
+++ src/characters/v1/mappers/character.mapper.ts
@@ -0,0 +1,30 @@
+import { CharacterType, SkillType } from '../../../constants';
+import { Character } from '../entities/character.entity';
+import { RelationsMapper } from './relations.mapper';
+import { SkillsMapper } from './skills.mapper';
+
+const map = (character: any, host: string): Character => {
+  return {
+    id: character.id,
+    name: character.name,
+    description: character.description,
+    imageUrl: character.image_url,
+    sourceUrl: character.source_url,
+    url: `${ host }/characters/${ character.id }`,
+    skills: {
+      bending: SkillsMapper.mapList(character.character_skills, SkillType.BENDING, host),
+      other: SkillsMapper.mapList(character.character_skills, SkillType.OTHER, host),
+    },
+    allies: RelationsMapper.mapList(character.relations, CharacterType.ALLY),
+    enemies: RelationsMapper.mapList(character.relations, CharacterType.ENEMY),
+  };
+}
+
+const mapList = (characters: any, host: string): Character[] => {
+  return characters.map((character) => map(character, host));
+}
+
+export const CharacterMapper = {
+  map,
+  mapList
+};
---
+++ src/characters/v1/mappers/index.ts
@@ -0,0 +1,3 @@
+export * from './character.mapper';
+export * from './relations.mapper';
+export * from './skills.mapper';
